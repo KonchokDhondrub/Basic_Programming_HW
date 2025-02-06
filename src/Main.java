b488b55551f1a03f8f6d721ff5fc1bffbf1ca4bb
kd <erm.melnik@gmail.com>
2025-02-06 19:55:07 +0100

    refactorings clearing code

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,24 @@
 //  5! = 1*2*3*4*5
 //  1! = 1
 //  9! = 1*2*3*4*5*6*7*8*9
-//Напишите рекурсивный метод, который считает факториал.
+//  Напишите рекурсивный метод, который считает факториал.
+
+//  b) (чуть сложнее) Последовательность Фибоначчи: 1,1,2,3,5,8,13 ...... Т.е. каждое следующее число это сумма двух предыдущих:
+//  5 это 2+3
+//  8 это 3+5
+//  13 это 5+8 и тд.
 
 public class Main {
     public static void main(String[] args) {
-        int factorial = 5;
-        System.out.println(factorial + "! = " + getFactorialRec(factorial));
+        int num = 5;
+        System.out.println(num + "! = " + getFactorialRec(num));
     }
 
     public static String getFactorialRec(int factorial) {
-        if (factorial < 0) {
-            return "";
+        if (factorial <= 0) {
+            return "error";
         }
-        if (factorial == 0 || factorial == 1) {
+        if (factorial == 1) {
             return "1";
         }
         return getFactorialRec(factorial - 1) + "*" + factorial;
